b471c771563855fe6ae59bffeebdcba3a347b855
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-19 23:53:48 +0100

    wrote the logic for the while loop

--- Binary_Search_Algorithm.py
+++ Binary_Search_Algorithm.py
@@ -1,20 +1,26 @@
-def binary_search(list, element):
-    start = 0
+def binary_search(dist, element):
     middle = 0
-    end = len(list)
+    start = 0
+    end = len(dist)
     steps = 0
 
-    while (start <= end):
-        print("step", steps, ":", str(list[start:end + 1]))
+    while start <= end:
+        print("Step", steps, ":", str(dist[start:end + 1]))
 
         steps = steps + 1
         middle = (start + end) // 2
 
-        if element == list(middle):
-           return middle
-        if element < list(middle):
-            return middle - 1
+        if element == dist[middle]:
+            return middle
+        if element < dist[middle]:
+            end = middle - 1
         else:
-            return middle + 1
+            end = middle + 1
 
     return -1
+
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+target = 2
+
+binary_search(my_list, target)
