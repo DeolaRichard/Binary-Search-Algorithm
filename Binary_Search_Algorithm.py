ae4c51ab7d90ccd67f44f012dd069fde0bdea23c
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-19 23:39:59 +0100

    defined the basic statements for the function

--- Binary_Search_Algorithm.py
+++ Binary_Search_Algorithm.py
@@ -3,3 +3,18 @@ def binary_search(list, element):
     middle = 0
     end = len(list)
     steps = 0
+
+    while (start <= end):
+        print("step", steps, ":", str(list[start:end + 1]))
+
+        steps = steps + 1
+        middle = (start + end) // 2
+
+        if element == list(middle):
+           return middle
+        if element < list(middle):
+            return middle - 1
+        else:
+            return middle + 1
+
+    return -1
